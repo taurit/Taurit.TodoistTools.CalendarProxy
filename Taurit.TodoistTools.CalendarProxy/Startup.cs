a297477485315dbd51051e9fa609a4a7648d7de9
Paweł Bulwan <github@taurit.pl>
2021-05-03 11:35:30 +0200

    Enable app insights to monitor perfrmance issue

--- Taurit.TodoistTools.CalendarProxy/Startup.cs
+++ Taurit.TodoistTools.CalendarProxy/Startup.cs
@@ -18,6 +18,7 @@ namespace Taurit.TodoistTools.CalendarProxy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry();
             services.AddControllersWithViews();
         }
 
@@ -31,8 +32,6 @@ namespace Taurit.TodoistTools.CalendarProxy
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
