0173e207b5e916a835c7422d9a492d5628f906b7
Paweł Bulwan <github@taurit.pl>
2023-03-22 17:19:56 +0100

    Duplicate proxy logic in AWS Lambda serverless application

--- Taurit.TodoistTools.CalendarProxy.Serverless/Functions.cs
+++ Taurit.TodoistTools.CalendarProxy.Serverless/Functions.cs
@@ -2,6 +2,10 @@ using Amazon.Lambda.Core;
 using Amazon.Lambda.Annotations;
 using Amazon.Lambda.Annotations.APIGateway;
 using System.Net;
+using System.Net.Http.Headers;
+using Taurit.TodoistTools.CalendarProxy.Library.Helpers;
+using EWSoftware.PDI.Parser;
+using Amazon.Lambda.APIGatewayEvents;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -15,12 +19,63 @@ namespace Taurit.TodoistTools.CalendarProxy.Serverless;
 public class Functions
 {
     [LambdaFunction()]
-    [HttpApi(LambdaHttpMethod.Get, "/filter/{calendarUrl}/{options}")]
-    public IHttpResult Filter(string calendarUrl, string options, ILambdaContext context)
+    [HttpApi(LambdaHttpMethod.Get, "/filter")]
+    public async Task<IHttpResult> Filter(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
     {
+        using HttpClient? webClient = new HttpClient()
+        {
+            DefaultRequestVersion = HttpVersion.Version11,
+            DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrHigher,
+            DefaultRequestHeaders =
+            {
+                // Office365 api started to failing with unclear 400 ArgumentNullException if User-Agent header was not present in the request
+                UserAgent = { new ProductInfoHeaderValue("HttpClient", "0.0.1") }
+            }
+        };
+        try
+        {
+            // parse request parameters and validate requested action
+            FilteringOptions? options = new FilteringOptions(request.RawQueryString);
+            //context.Logger.LogInformation($"Received raw query string: {request.RawQueryString}");
 
+            if (options.CalendarUrl == null)
+                throw new ArgumentException("calendarUrl parameter was not provided by the user");
 
-        return HttpResults.Ok($"Calendar url is {calendarUrl}, option string is {options}");
+            if (!(options.CalendarUrl.Scheme.ToLowerInvariant() == "https" ||
+                  options.CalendarUrl.Scheme.ToLowerInvariant() == "http"))
+                return HttpResults.BadRequest("Specified protocol was not recognized. Url should begin with 'http' or 'https'.");
+
+            // download the source iCalendar file content
+            string icalContent = await webClient.GetStringAsync(options.CalendarUrl);
+
+            // parse iCalendar and filter according to user-defined options
+            EventManager eventManager = new EventManager(icalContent);
+            eventManager.Filter(options);
+
+            // return filtered calendar as a response in iCalendar format
+            string? icalResponse = eventManager.GetIcal();
+            return HttpResults.Ok(icalResponse);
+        }
+        catch (ArgumentException ae)
+        {
+            return HttpResults.BadRequest(ae.Message);
+        }
+        catch (UriFormatException)
+        {
+            return HttpResults.BadRequest("Specified URL was not valid.");
+        }
+        catch (WebException)
+        {
+            return HttpResults.BadRequest("Specified resource could not have been accessed.");
+        }
+        catch (PDIParserException)
+        {
+            return HttpResults.BadRequest("Requested file is not a valid iCalendar file.");
+        }
+        catch (Exception)
+        {
+            return HttpResults.InternalServerError("Application exception");
+        }        
     }
 
 }
