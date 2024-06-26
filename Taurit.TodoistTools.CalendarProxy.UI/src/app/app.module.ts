6ed99657a2534e200f8b4a17ac7e6413605018e3
Paweł Bulwan <github@taurit.pl>
2024-06-26 10:02:09 +0200

    Auto-upgrade Angular to v18

--- Taurit.TodoistTools.CalendarProxy.UI/src/app/app.module.ts
+++ Taurit.TodoistTools.CalendarProxy.UI/src/app/app.module.ts
@@ -1,17 +1,11 @@
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule, HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
