b8e2a6c1d1c6b0dd220d846fe7f70c33309ce205
Paweł Bulwan <github@taurit.pl>
2025-01-01 09:35:13 +0100

    Bump angular version

--- Taurit.TodoistTools.CalendarProxy.UI/src/app/app.component.ts
+++ Taurit.TodoistTools.CalendarProxy.UI/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   title = 'Taurit.TodoistTools.CalendarProxy.UI';
